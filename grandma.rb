fff0029d46a51e5bf4793c6c49163c0bdd9b53ed
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-09 19:02:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,16 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
+  love = "I LOVE YOU TOO PUMPKIN!"
   hear = "NO, NOT SINCE 1938!"
   cant_hear = "HUH?! SPEAK UP, SONNY!"
-  if string == string.upcase
+  if string == "I LOVE YOU GRANDMA!"
+    love
+  elsif
+    string == string.upcase
     hear
   else
     cant_hear
-  end  
+  end
 end
 
 # Whatever you say to grandma, she should respond with
